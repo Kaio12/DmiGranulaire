cb0f5340e59374397b51d1278482e714a38c9d13
Philippe Caillot <philippe@caillot.ovh>
2025-03-10 10:10:17 +0100

    à partir de la piste distribute, dispatche les grains selon leur position dans un array 8*8

--- construcGrille.js
+++ construcGrille.js
@@ -22,15 +22,10 @@ if (mubu != null)
                 var descr = mubu.gettrack(1, "descr"); // se réfère à descr
                 if(descr != null)
                         {
-                        
                         var markers = mubu.gettrack(1, "markers"); // se réfère à la piste markers
-                        
-                        post("distribute ok\n");
-                        post("cols ", distribute.mxcols, "\n");
-                        post("rows ", distribute.mxrows, "\n");
 
-                        var config = " @name dispatchGrains @matrixcolnames pad begin maxLoudTime periodicity ";
-	                grille = mubu.addtrack(1000, 4, 1, config);//(maxsize, mxrows, mxcols, configsym
+                        var config = " @name dispatchGrains @matrixcolnames pad attackTime attackDuration maxLoudTime releaseDuration endRelease periodicity ";
+	                grille = mubu.addtrack(1000, 7, 1, config);//(maxsize, mxrows, mxcols, configsym
                         grille.clear(); // on efface préalablement la piste partielsSynth à l'appel de la fonction
         
                         var DistX = [];
@@ -44,13 +39,18 @@ if (mubu != null)
                         var pad = 0;
                         var indX = 0;
                         var indY = 0;
-                        var timetag = 0;
+                        var attackTime = 0;
+                        var attackDuration = 0;
+                        var releaseDuration = 0;
+                        var durations = []; // les durées de chaque grain
+                        var dureeGrain = 0;
+
 
                         DistX = distribute.getmxcolumn(1); // récupère les positions x des grains
                         DistY = distribute.getmxcolumn(2); // récupère les positions y des grains
-
-                        post(DistX, "\n");
-                        post(DistY, "\n"); 
+                        
+                        durations = markers.getmxcolumn(0); // récupère la durée de chaque grain
+                        post("durations ", durations, "\n");
 
                         minX = Math.min(...DistX);
                         maxX = Math.max(...DistX);
@@ -59,19 +59,35 @@ if (mubu != null)
         
                         for (let i = 0; i < DistX.length; i++)
                                 {
-                                timetag = markers.gettime (i);   
+                                dureeGrain = durations[i];
+                                //post("dureeGrain ", dureeGrain, "\n");
+
+                                attackTime = markers.gettime (i); 
+                                //post("attackTime ", i, " ", attackTime, "\n");
+                                
                                 maxLoudTime = partielsSynth.gettime(i);
-                                post("maxLoudTime", maxLoudTime, "\n");
+                                //post("maxLoudTime ",  i, " ",maxLoudTime, "\n");
+
+                                attackDuration = maxLoudTime - attackTime;
+                                //post("attackDuration ", attackDuration, "\n");
+                                
+                                releaseDuration = + attackTime + dureeGrain - maxLoudTime;
+                                //post("releaseDuration ", releaseDuration, "\n");
+                                
+                                endRelease = + maxLoudTime + releaseDuration;
+                                //post("endRelease ", endRelease, "\n");
+
                                 Periodicity = descr.getmxcolumn(3)[i]; // récupère Periodicity de descr -> qualité noise/périodicité
-                                post("Periodicity", Periodicity, "\n");
+                                
+                                // construit la correspondance avec la note midi sur le launchpad
                                 indX = Math.trunc(((DistX[i] - minX) / (maxX - minX)) * 8) + 1;
                                 if(indX > 8) indX = 8; // on ne peut pas dépasser 8, pour le grain qui est à la position max
                                 indY = Math.trunc(((DistY[i] - minY) / (maxY - minY)) * 8) + 1;
                                 if(indY > 8) indY = 8;
-                                pad = indY * 10 + indX; // construit la correspondance avec la note midi sur le launchpad
-                                post("indice", pad, "\n");
-                                post("DistX", DistX[i], "DistY", DistY[i], "\n");
-                                grille.append(pad, timetag, maxLoudTime, Periodicity);
+                                pad = indY * 10 + indX;
+
+                                //remplissage de la grille avec les données colléctées
+                                grille.append(pad, attackTime, attackDuration, maxLoudTime, releaseDuration, endRelease,Periodicity);
                                 }   
                         }
 
@@ -79,4 +95,6 @@ if (mubu != null)
                 }
         }  
 }
+outlet(0, "bang");
+
 }
